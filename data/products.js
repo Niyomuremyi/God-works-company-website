2b230f0d0a0edf287596910202d66c7c2b122396
Niyomuremyi Elissa <niyomulemyi@gmail.com>
2026-03-03 00:14:17 +0800

    restructuring , renaming the files and even making changes in the codes especially terminology of class

--- data/products.js
+++ data/products.js
@@ -1,5 +1,62 @@
-const products = [
+export const products = [
+  { 
+    id:"3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+    image:"images/products/electric-kettle.jpg",
+    name:"Fast Boil Electric kettle",
+    rating:{
+      stars:4.5,
+      count:10
+    },
+    priceCents:20
+  },
   {
-    name: "",
+    id:"e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image:"images/products/honey(1kg).jpg",
+    name:"Natural honey(1kg)",
+    rating:{
+      stars:4,
+      count:7
+    },
+    priceCents:15
+  },
+  {
+    id:"15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    image:"images/products/jug-glass.jpg",
+    name:"Glass Jug",
+    rating:{
+      stars:5,
+      count:6
+    },
+    priceCents:7
+  },
+  {
+    id:"83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image:"images/products/mug.jpg",
+    name:"Tea Mug",
+    rating:{
+      stars:4.5,
+      count:9
+    },
+    priceCents:2
+  },
+  {
+    id:"54e0eccd-8f36-462b-b68a-8182611d9add",
+    image:"images/products/plates.jpg",
+    name:"Breakfast Plates",
+    rating:{
+      stars:5,
+      count:8
+    },
+    priceCents:3
+  },
+  {
+    id:"3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    image:"images/products/rice(25kg).webp",
+    name:"Premium Rice(25kg)",
+    rating:{
+      stars:4,
+      count:6
+    },
+    priceCents:30
   },
 ];
---
+++ scripts/God-works.js
@@ -0,0 +1,24 @@
+import {canvas} from "./canvas.js";
+import {products} from "../data/products.js"
+
+canvas();
+
+let productsHTML = '';
+
+products.forEach((product)=>{
+  productsHTML +=`
+  <div class="product-container">
+            <img class="product-image" src=${product.image} alt="">
+                <div class="product-info">${product.name}</div>
+            <div class="product-rating">
+            <div class="product-rating-container">
+                <img class="product-rating-stars"  src="images/ratings/rating-${product.rating.stars * 10}.png" alt=""></div>
+            <div class="product-rating-count">${product.rating.count}</div>
+             </div>
+             <div class="product-price">$${product.priceCents}</div>
+            <div class="added-to-cart">
+                <button class="add-to-cart-button">add to cart</button></div>
+        </div>
+  `
+});
+document.querySelector('.products-grid').innerHTML = productsHTML;
